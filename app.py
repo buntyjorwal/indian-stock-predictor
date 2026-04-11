1ed93c8bd2996f73c8b3a03f6bca3bae86799825
Bunty Jorwal <buntyjorwal@gmail.com>
2026-04-11 13:20:35 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def inject_css() -> None:
             }
 
             .block-container {
-                max-width: 1500px;
+                max-width: 1800px;
                 padding-top: 0.65rem;
                 padding-bottom: 1.75rem;
             }
